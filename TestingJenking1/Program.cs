50964853d707f04092cbea5b8307256c9763de5f
fnursena <nursena.cavli@hotmail.com>
2022-09-13 14:33:10 +0300

    Proje dosyası ekle.

---
+++ TestingJenking1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingJenking1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
